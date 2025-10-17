b0ac7740c99b681cf726b4796a4667143963c935
xiaoyang <1611037570@qq.com>
2025-10-17 09:05:36 +0800

    feat: :sparkles: 优化相关搜索推荐

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # snowflake-index
 
-项目使用pnpm作为包管理器 推荐用vscode作为编辑器
+这一款带着摸鱼属性的浏览器起始页
+今日小雪，2025年11月22日开源并发布在github上
+
+项目技术栈
+开发语言：vue
+脚手架：vite
+组件库：element-plus
+包管理器：pnpm
+开发工具：trae
--- src/views/index/components/search.vue
+++ src/views/index/components/search.vue
@@ -4,20 +4,30 @@ import { storeToRefs } from 'pinia'
 import { ref } from 'vue'
 const indexStore = useIndexStore()
 const { searchFocus } = storeToRefs(indexStore)
-function translate(text) {
+function translateSearch() {
   // 参考url
   // https://fanyi.baidu.com/mtpe-individual/transText?query=I%20don%27t%20know&lang=en2zh#/
   // https://fanyi.baidu.com/mtpe-individual/transText#/zh/en/%E4%B8%BA%E4%BB%80%E4%B9%88
   const BASE_URL = 'https://fanyi.baidu.com/mtpe-individual/transText'
   const chineseCharRegex = /[\u4e00-\u9fa5\u3400-\u4dbf\u{20000}-\u{2a6df}]/u
-  const fromLang = chineseCharRegex.test(text) ? 'en' : 'zh'
+  const fromLang = chineseCharRegex.test(searchText.value) ? 'en' : 'zh'
 
-  const encodedText = encodeURIComponent(text)
+  const encodedText = encodeURIComponent(searchText.value)
   const hash = `#/auto/${fromLang}/${encodedText}`
 
   window.open(BASE_URL + hash)
 }
 const searchText = ref('')
+
+function githubSearch() {
+  window.open(
+    'https://github.com/search?q=' + encodeURIComponent(searchText.value) + '&type=repositories',
+  )
+}
+// 文心一言搜索
+function aiSearch() {
+  window.open('https://chat.baidu.com/search?word=' + encodeURIComponent(searchText.value))
+}
 
 const handleSearch = () => {
   window.open('https://www.baidu.com/s?ie=utf-8&wd=' + searchText.value)
@@ -28,6 +38,21 @@ const handleFocus = () => {
 const handleBlur = () => {
   searchFocus.value = false
 }
+
+const list = ref([
+  {
+    name: '文心一言',
+    fn: aiSearch,
+  },
+  {
+    name: '翻译',
+    fn: translateSearch,
+  },
+  {
+    name: 'github',
+    fn: githubSearch,
+  },
+])
 </script>
 
 <template>
@@ -50,8 +75,15 @@ const handleBlur = () => {
         搜索
       </div>
     </div>
-    <div v-if="searchText" class="w-full bg-amber-200" @click="translate(searchText)">
-      翻译：{{ searchText }}
+    <div v-if="searchText && searchFocus" class="w-full bg-amber-200 flex flex-col z-30 p-3">
+      <div
+        @click="item.fn"
+        v-for="item in list"
+        :key="item.name"
+        class="p-3 cursor-pointer hover:bg-[#e5e5e5] whitespace-nowrap overflow-hidden text-ellipsis"
+      >
+        {{ item.name }}：{{ searchText }}
+      </div>
     </div>
   </div>
 </template>
